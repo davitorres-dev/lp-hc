bdeaa82b40ec67fd8edfc29860d738c36b5a3049
Davi Torres <83573302+davitorres-dev@users.noreply.github.com>
2025-06-14 03:57:33 +0000

    update

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,4 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-// Exporta função para montar em um container customizado (ex: Shadow DOM)
-export default function mountReactApp(container: HTMLElement) {
-  createRoot(container).render(<App />);
-}
-
-// Permite rodar normalmente se tiver um #root no DOM principal
-const rootEl = document.getElementById("root");
-if (rootEl) {
-  mountReactApp(rootEl);
-}
-
+createRoot(document.getElementById("root")!).render(<App />);
